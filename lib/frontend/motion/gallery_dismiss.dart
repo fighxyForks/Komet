f479e9d566b4171eedf9a92e647a89d7535334d9
Ivan K <57476075+fighxy@users.noreply.github.com>
2026-09-24 21:02:37 +0700

    chore(motion): drop unused physics imports after SettlingSpring
    Gallery dismiss and photo hero import springs via ios_motion only.

--- lib/frontend/motion/gallery_dismiss.dart
+++ lib/frontend/motion/gallery_dismiss.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/physics.dart';
 
 import 'ios_motion.dart';
 
--- lib/frontend/widgets/attachment/photo_hero.dart
+++ lib/frontend/widgets/attachment/photo_hero.dart
@@ -3,7 +3,6 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/physics.dart';
 
 import '../../motion/ios_motion.dart';
 
